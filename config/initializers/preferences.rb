44d0993fd7d501de8cf4b2bf13e743c3e604e1a5
Marcelo Elizeche <melizeche@gmail.com>
2014-07-08 02:35:37 -0400

    arreglando el commit

--- config/initializers/preferences.rb
+++ config/initializers/preferences.rb
@@ -2,5 +2,5 @@
 
 # All Legislative preferences are here.
 
-Legislative::Application.config.application_name = 'Congreso abierto'
-Legislative::Application.config.created_by_name = 'Fundación Ciudadano Inteligente'
+Legislative::Application.config.application_name = 'Parlamento Abierto'
+Legislative::Application.config.created_by_name = 'TEDIC'
